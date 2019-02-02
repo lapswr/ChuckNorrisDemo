0867fbceaa0d6b8484778c851570a1411c540e0a
lapswr <lapswr@gmail.com>
2019-02-02 16:26:17 +0000

    Add the app tittle in base layout

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,7 +6,7 @@
         <!-- provide the csrf token -->
         <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-        <title>NorrisDemo - @yield('title')</title>
+        <title>ChuckNorrisDemo - @yield('title')</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
@@ -77,20 +77,10 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            {{--@if (Route::has('login'))--}}
-                {{--<div class="top-right links">--}}
-                    {{--@auth--}}
-                        {{--<a href="{{ url('/home') }}">Home</a>--}}
-                    {{--@else--}}
-                        {{--<a href="{{ route('login') }}">Login</a>--}}
-                        {{--@if (Route::has('register'))--}}
-                            {{--<a href="{{ route('register') }}">Register</a>--}}
-                        {{--@endif--}}
-                    {{--@endauth--}}
-                {{--</div>--}}
-            {{--@endif--}}
-
             <div class="content">
+                <div class="title m-b-md">
+                    Chuck Norris Jokes
+                </div>
                 @yield('content')
             </div>
         </div>
